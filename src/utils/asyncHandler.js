2ca97a71cda5bed1704fbcc0a505b63eca929f49
alokkumarsingh07 <kalok8841@gmail.com>
2024-08-12 12:09:44 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -20,3 +20,14 @@ export { asyncHandler };
 //         })
 //     }
 // }
+// const asyncHandler = (requestHandler) => async (req, res, next) => {
+//     try {
+//         await requestHandler(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+// export { asyncHandler };
