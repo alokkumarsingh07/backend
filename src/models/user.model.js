10aa3ae90ffade2cf59fea7ba49ac2acead8230d
alokkumarsingh07 <kalok8841@gmail.com>
2024-08-17 12:22:27 +0530

    User model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -53,16 +53,16 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-userSchema.methods.isPasswordCorrect = async (password) => {
-  return await bcrypt.compare(password, this.password);
-};
+userSchema.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(password, this.password)
+}
 
 userSchema.methods.generateAccessToken = () => {
   return jwt.sign(
     {
       _id: this._id,
       email: this.email,
-      username: this.username,
+      userName: this.userName,
       fullName: this.fullName,
     },
     process.env.ACCESS_TOKEN_SECRET,
